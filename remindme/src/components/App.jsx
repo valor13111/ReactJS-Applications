9d154dfb11223cfb56a70d16a5e838c6858d8d2b
Tyler Reed <valor13111@gmail.com>
2017-11-24 17:27:36 -0500

    Updated with constants folder and REDUX.

--- remindme/src/components/App.jsx
+++ remindme/src/components/App.jsx
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addReminder } from '../actions';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
+  addReminder() {
+    console.log('this', this);
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,13 +27,17 @@ class App extends Component {
             <input
               className="form-control"
               placeholder="I have to..."
+              onChange={event => this.setState({
+                text: event.target.value,
+              })}
             />
           </div>
           <button
             type="button"
             className="btn btn-success"
+            onClick={() => this.addReminder()}
           >
-          Add Reminder
+            Add Reminder
           </button>
         </div>
       </div>
@@ -26,4 +45,8 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({addReminder}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(App);
